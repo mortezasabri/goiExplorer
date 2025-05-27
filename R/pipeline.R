0efb5c23ea0c949e1e1b4746aa9ad2729c3f6c10
Morteza Sabri <35361537+mortezasabri@users.noreply.github.com>
2025-05-27 12:01:33 +0200

    indent

--- R/pipeline.R
+++ R/pipeline.R
@@ -3,7 +3,7 @@
 #' Reads in a plain counts table plus metadata, performs DESeq2 analysis,
 #' enrichment, and writes results to disk.
 #'
-#' @param dds a `dds` object from DESeq2 
+#' @param dds a `dds` object from DESeq2
 #' @param goi a gene of interest
 #' @param parent_outdir Character. Directory where all outputs (tables, plots) will be written.
 #' If it does not exist, it will be created recursively.
@@ -30,7 +30,7 @@
 #' \dontrun{
 #' pipeline_counts(
 #'   dds = "inst/extdata",
-#'   outdir   = "counts_output"
+#'   outdir = "counts_output"
 #' )
 #' }
 #' @importFrom DESeq2 resultsNames DESeq results counts plotMA
@@ -46,7 +46,7 @@
 #' @export
 pipeline <- function(dds,
                      goi,
-                     parent_outdir, 
+                     parent_outdir,
                      abr_healthy,
                      abr_case,
                      ensemblSpecies,
@@ -71,25 +71,25 @@ pipeline <- function(dds,
   stopifnot(inherits(pCutoff, "numeric"))
   stopifnot(inherits(pAdjustMethod, "character"))
   stopifnot(inherits(palette, "character"))
-  
+
   if (ensemblSpecies == "hsapiens_gene_ensembl") {
-    organism = "human"
-    ScientificName = "Homo sapiens"
-    abrScientificName = "hsa"
+    organism <- "human"
+    ScientificName <- "Homo sapiens"
+    abrScientificName <- "hsa"
   } else {
-    stop('ensemblSpecies should be one of: \n 
+    stop('ensemblSpecies should be one of: \n
          biomaRt::listDatasets(biomaRt::useEnsembl("ensembl"))$dataset \n
          and for now the only valid value is "hsapiens_gene_ensembl"')
   }
-  
+
   res_output <- list() # saving the directories and results in this as an output of run_pipeline()
   res_output$goi <- goi
   res_output$ensemblSpecies <- ensemblSpecies
-  
 
-  
-  
-  
+
+
+
+
   # ensure we have a real output directory
   if (missing(parent_outdir) || is.null(parent_outdir)) {
     parent_outdir <- file.path(tempdir(), paste0("goiExplorer_", Sys.time()))
@@ -97,71 +97,77 @@ pipeline <- function(dds,
   if (!dir.exists(parent_outdir)) dir.create(parent_outdir, recursive = TRUE)
   outdir <- parent_outdir
   res_output$directory <- outdir
-  
+
   # Try the *default* Ensembl site first, then fall back to a cached file
   mart_path <- system.file("extdata", "hsa_mart.rds", package = "goiExplorer")
   mart <- tryCatch(
     biomaRt::useEnsembl("ensembl", dataset = ensemblSpecies),
     error = function(e_live) {
       message("Default Ensembl failed, trying US mirror")
-      warning("Live Ensembl query failed: ", e_live$message,
-            "\nFalling back to local cache in extdata.")
+      warning(
+        "Live Ensembl query failed: ", e_live$message,
+        "\nFalling back to local cache in extdata."
+      )
       # Fall back to your shipped RDS
       base::readRDS(mart_path)
     }
   )
-  
+
   ## ----DESeq2
   # The condition of interest should go at the end
   message("Number of genes before omiting low expressed genes: ")
   nrow(dds)
   keep <- rowSums(DESeq2::counts(dds) >= 10) >= 1
-  dds <- dds[keep,]
+  dds <- dds[keep, ]
   message("Number of genes after omiting low expressed genes: ")
   nrow(dds)
-  
+
   dds <- suppressMessages(DESeq2::DESeq(dds))
   dds <- DESeq2::estimateSizeFactors(dds)
   counts <- DESeq2::counts(dds)
 
   res_output$dds <- dds
   res_output$counts <- counts
-  
+
   # ----------- DE
   DESeq2::resultsNames(dds)
   message("Summary of differentially expressed genes: ")
   summary(res <- DESeq2::results(dds, alpha = pCutoff, pAdjustMethod = pAdjustMethod))
   res_output$res <- res
-  
-  
+
+
   # ----------- Normalization
   vsd <- DESeq2::vst(dds, blind = FALSE)
   res_output$vsd <- vsd
-  
-  
+
+
   ## ----DESeq2 out
   res.df <- get_res_df(dds, rowNamesOfCounts, mart, pCutoff, pAdjustMethod)
   res$pvalue <- ifelse(is.na(res$pvalue), 1, res$pvalue)
   res$padj <- ifelse(is.na(res$padj), 1, res$padj)
   res.df$pvalue <- ifelse(is.na(res.df$pvalue), 1, res.df$pvalue)
   res.df$padj <- ifelse(is.na(res.df$padj), 1, res.df$padj)
-  
+
   # degs: only those passed the cutoffs
   degs <- subset(res.df, padj < pCutoff & abs(log2FoldChange) > lfcCutoff)
-  write_txt_xlsx(degs, outdir, 
-                 prefix = paste0("degs_", abr_case, "_Vs_", abr_healthy))
-  write_txt_xlsx(res.df, outdir, 
-                 prefix = paste0("de_", abr_case, "_Vs_", abr_healthy))
-  
+  write_txt_xlsx(degs, outdir,
+    prefix = paste0("degs_", abr_case, "_Vs_", abr_healthy)
+  )
+  write_txt_xlsx(res.df, outdir,
+    prefix = paste0("de_", abr_case, "_Vs_", abr_healthy)
+  )
+
   res_output$degs <- degs
   res_output$res.df <- res.df
-  
+
   ## ----abstract
-  attributes <- c(rowNamesOfCounts, "external_gene_name",
-                                       'ensembl_gene_id', 
-                                       'gene_biotype', "description", 
-                                       'chromosome_name', 
-                                       'start_position', 'end_position', 'strand')
+  attributes <- c(
+    rowNamesOfCounts, "external_gene_name",
+    "ensembl_gene_id",
+    "gene_biotype", "description",
+    "chromosome_name",
+    "start_position", "end_position", "strand"
+  )
   des <- tryCatch(
     biomaRt::getBM(
       attributes = attributes,
@@ -175,226 +181,279 @@ pipeline <- function(dds,
         "  skipping annotation."
       )
       # Return an empty data.frame with the right columns
-      base::data.frame(base::matrix(ncol = base::length(attributes), nrow = 0,
-                        dimnames = base::list(NULL, attributes)),
-                stringsAsFactors = FALSE)
+      base::data.frame(
+        base::matrix(
+          ncol = base::length(attributes), nrow = 0,
+          dimnames = base::list(NULL, attributes)
+        ),
+        stringsAsFactors = FALSE
+      )
     }
   )
 
-  entrezgene_id <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, 
-                                         keys = goi, 
-                                         keytype = "SYMBOL", column="ENTREZID")
-  
-  link <- unique(paste0("https://useast.ensembl.org/", 
-                        sub(" ", "_", ScientificName),
-                        "/Gene/Summary?g=", des$ensembl_gene_id))
+  entrezgene_id <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
+    keys = goi,
+    keytype = "SYMBOL", column = "ENTREZID"
+  )
+
+  link <- unique(paste0(
+    "https://useast.ensembl.org/",
+    sub(" ", "_", ScientificName),
+    "/Gene/Summary?g=", des$ensembl_gene_id
+  ))
   res_output$desGOI <- des
-  
-  
+
+
   df <- geneCounts <-
     DESeq2::plotCounts(
       dds,
       gene = goi,
       intgroup = intgroup,
-      returnData = TRUE)
+      returnData = TRUE
+    )
   res_output$geneCounts <- geneCounts
-  
-  
+
+
   ## ----Barplot
   stat.test <- df %>% rstatix::t_test(count ~ condition)
   stat.test$p <- base::round(res.df[goi, ]$padj, 3)
   stat.test$statistic <- base::round(res.df[goi, ]$stat, 3)
   stat.test <- rstatix::add_significance(stat.test)
   stat.test.mean <- stat.test %>% add_xy_position(fun = "mean_sd", x = intgroup)
-  
-  g <- ggbarplot(df, x = intgroup, y = "count", add = "mean_sd",
-                         fill = intgroup, 
-                         palette = palette) + 
-    stat_pvalue_manual(stat.test.mean, label = "padj: {p} {p.signif}",
-                               tip.length = 0.01) +
+
+  g <- ggbarplot(df,
+    x = intgroup, y = "count", add = "mean_sd",
+    fill = intgroup,
+    palette = palette
+  ) +
+    stat_pvalue_manual(stat.test.mean,
+      label = "padj: {p} {p.signif}",
+      tip.length = 0.01
+    ) +
     labs(title = paste0("Barplot for ", goi), y = "Normalized counts")
   p <- paste0(outdir, "barplot_", goi, ".png")
   ggsave(p,
-                  g,
-                  dpi = 300)
+    g,
+    dpi = 300
+  )
   res_output$Barplot <- g
   res_output$BarplotPath <- p
-  
-  
+
+
   ## ----Boxplot
   stat.test.max <- stat.test %>% add_xy_position(x = intgroup)
-  
-  rm(g); g <- ggpubr::ggboxplot(df, x = intgroup, y = "count", 
-                                fill = intgroup, 
-                                palette = palette) +
-    ggplot2::geom_point(position = ggplot2::position_dodge(width=0.75), 
-                        ggplot2::aes(group=condition), 
-                        color = "steelblue4") + 
-    ggpubr::stat_pvalue_manual(stat.test.max, label = "padj: {p} {p.signif}",
-                               vjust = -1, bracket.nudge.y = 1) +
-    ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.05, 0.15)))  + 
+
+  rm(g)
+  g <- ggpubr::ggboxplot(df,
+    x = intgroup, y = "count",
+    fill = intgroup,
+    palette = palette
+  ) +
+    ggplot2::geom_point(
+      position = ggplot2::position_dodge(width = 0.75),
+      ggplot2::aes(group = condition),
+      color = "steelblue4"
+    ) +
+    ggpubr::stat_pvalue_manual(stat.test.max,
+      label = "padj: {p} {p.signif}",
+      vjust = -1, bracket.nudge.y = 1
+    ) +
+    ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.05, 0.15))) +
     ggplot2::labs(title = paste0("Boxplot for ", goi), y = "Normalized counts")
   p <- paste0(outdir, "boxplot_", goi, ".png")
   ggplot2::ggsave(p,
-                  g,
-                  dpi = 300)
+    g,
+    dpi = 300
+  )
   res_output$Boxplot <- g
   res_output$BoxplotPath <- p
-  
-  
+
+
   ## ----Count Plot
-  rm(g); g <- ggplot2::ggplot(geneCounts, 
-                              ggplot2::aes(x = condition, y = count, color = condition)) +
-    ggplot2::scale_y_log10() + ggbeeswarm::geom_beeswarm(cex = 3) + 
+  rm(g)
+  g <- ggplot2::ggplot(
+    geneCounts,
+    ggplot2::aes(x = condition, y = count, color = condition)
+  ) +
+    ggplot2::scale_y_log10() +
+    ggbeeswarm::geom_beeswarm(cex = 3) +
     ggplot2::ggtitle(goi) +
     ggplot2::theme(legend.position = "none")
   p <- paste0(outdir, "plotCounts_", goi, ".png")
   ggplot2::ggsave(p,
-                  g,
-                  dpi = 300)
+    g,
+    dpi = 300
+  )
   res_output$Countplot <- g
   res_output$CountplotPath <- p
-  
+
   ## ----Volcano
   res.df$threshold <- res.df$padj < pCutoff & abs(res.df$log2FoldChange) > lfcCutoff
   res.df$genelabels <- base::rownames(res.df) %in% goi # name of genes want to display
   base::options(ggrepel.max.overlaps = Inf) # in case of ggrepel: 1 unlabeled data points (too many overlaps). Consider increasing max.overlaps
-  
-  
-  rm(g); g <- ggplot2::ggplot(res.df) +
-    ggplot2::geom_point(ggplot2::aes(x = log2FoldChange, y = -log10(padj), colour = threshold), 
-                        size =0.4) +
-    ggrepel::geom_text_repel(aes(x = log2FoldChange, y = -log10(padj), 
-                                 label = ifelse(genelabels == T, 
-                                                base::rownames(res.df),""))) +
-    ggplot2::ggtitle( paste0("Volcano Plot (", goi, ")")) +
-    ggplot2::xlab("log2 fold change") + 
+
+
+  rm(g)
+  g <- ggplot2::ggplot(res.df) +
+    ggplot2::geom_point(ggplot2::aes(x = log2FoldChange, y = -log10(padj), colour = threshold),
+      size = 0.4
+    ) +
+    ggrepel::geom_text_repel(aes(
+      x = log2FoldChange, y = -log10(padj),
+      label = ifelse(genelabels == T,
+        base::rownames(res.df), ""
+      )
+    )) +
+    ggplot2::ggtitle(paste0("Volcano Plot (", goi, ")")) +
+    ggplot2::xlab("log2 fold change") +
     ggplot2::ylab("-log10 adjusted p-value") +
-    ggplot2::theme(legend.position = "none",
-                   plot.title = ggplot2::element_text(size = ggplot2::rel(1.5), hjust = 0.5),
-                   axis.title = ggplot2::element_text(size = ggplot2::rel(1.25))) 
+    ggplot2::theme(
+      legend.position = "none",
+      plot.title = ggplot2::element_text(size = ggplot2::rel(1.5), hjust = 0.5),
+      axis.title = ggplot2::element_text(size = ggplot2::rel(1.25))
+    )
   p <- paste0(outdir, "volcanoPlot_", goi, ".png")
   ggplot2::ggsave(p,
-                  g,
-                  dpi = 300)
+    g,
+    dpi = 300
+  )
   res_output$Volcanoplot <- g
   res_output$VolcanoplotPath <- p
-  
+
   ## ----MA plot
   df <- base::subset(base::as.data.frame(res), base::rownames(df) %in% goi)
   p <- base::paste0(outdir, "MAplot_", goi, ".png")
   grDevices::png(p, units = "in", height = 10, width = 14, res = 200)
-  DESeq2::plotMA(res, ylim = c(-4,4), main = "MA Plot")
+  DESeq2::plotMA(res, ylim = c(-4, 4), main = "MA Plot")
   if (nrow(df) > 0) {
-    graphics::text(df$baseMean, df$log2FoldChange, goi, pos = 2, col = "black", cex = 1) 
+    graphics::text(df$baseMean, df$log2FoldChange, goi, pos = 2, col = "black", cex = 1)
   }
   grDevices::dev.off()
   res_output$plotMApath <- p
-  
+
   ## ----DEGsToDiseases
   x <- base::data.frame(KnowSeq::DEGsToDiseases(goi, size = 10, getEvidences = TRUE))[, 1:2]
-  x[,2] <- base::as.numeric(x[,2])
-  x[,2] <- base::round(x[,2], 3)
+  x[, 2] <- base::as.numeric(x[, 2])
+  x[, 2] <- base::round(x[, 2], 3)
   write_txt_xlsx(x, outdir,
-                 prefix = paste0("DEGsToDiseases_", goi))
+    prefix = paste0("DEGsToDiseases_", goi)
+  )
   res_output$DEGsToDiseases <- x
-  
-  
-  
+
+
+
   ## ----Pathways
   outKEGG <- paste0(outdir, "KEGG/")
   if (!dir.exists(outKEGG)) dir.create(outKEGG, recursive = TRUE)
   tmp <- getwd()
   setwd(outKEGG)
-  
-  geneList <- degs$log2FoldChange; names(geneList) <- degs$entrezgene_id
+
+  geneList <- degs$log2FoldChange
+  names(geneList) <- degs$entrezgene_id
   geneList <- geneList[stats::complete.cases(names(geneList))]
   geneList <- geneList[order(geneList, decreasing = T)]
 
   z <- limma::getGeneKEGGLinks(abrScientificName)
   path.ids <- z[z$GeneID == entrezgene_id, ]$PathwayID
-  
+
   for (i in 1:length(path.ids)) {
     names(path.ids)[i] <- KEGGREST::keggGet(path.ids[i])[[1]]$PATHWAY
   }
-  
+
   for (i in 1:length(path.ids)) {
-    tryCatch({
-      var2 <- path.ids[i]
-      nm <- paste0(var2, ": ", names(var2))
-      message(paste0("\n *** Initiating loop ", i, "/",length(path.ids), ": ", nm))
-      
-      geneOnThePathID <- KEGGREST::keggGet(path.ids[i])[[1]]$GENE
-      geneOnThePathID <- geneOnThePathID[seq(1, length(geneOnThePathID), by = 2)]
-      geneOnThePathID <- geneList[which(names(geneList) %in% geneOnThePathID)]
-      ceil <- max(abs(geneOnThePathID))
-      if (requireNamespace("pathview", quietly = TRUE)) {
-        tryCatch({
-          pathview(
-            gene.data = geneList,
-            pathway.id = path.ids[i],
-            species = abrScientificName,
-            out.suffix = names(var2),
-            kegg.native = T,
-            limit = list(gene = ceil, cpd = 1)
+    tryCatch(
+      {
+        var2 <- path.ids[i]
+        nm <- paste0(var2, ": ", names(var2))
+        message(paste0("\n *** Initiating loop ", i, "/", length(path.ids), ": ", nm))
+
+        geneOnThePathID <- KEGGREST::keggGet(path.ids[i])[[1]]$GENE
+        geneOnThePathID <- geneOnThePathID[seq(1, length(geneOnThePathID), by = 2)]
+        geneOnThePathID <- geneList[which(names(geneList) %in% geneOnThePathID)]
+        ceil <- max(abs(geneOnThePathID))
+        if (requireNamespace("pathview", quietly = TRUE)) {
+          tryCatch(
+            {
+              pathview(
+                gene.data = geneList,
+                pathway.id = path.ids[i],
+                species = abrScientificName,
+                out.suffix = names(var2),
+                kegg.native = T,
+                limit = list(gene = ceil, cpd = 1)
+              )
+            },
+            error = function(e) {
+              warning("Could not generate KEGG plot for ", names(var2), ": ", e$message)
+            }
           )
-        }, error = function(e) {
-          warning("Could not generate KEGG plot for ", names(var2), ": ", e$message)
-        })
-      } else {
-        warning("pathview package not installed; skipping KEGG plots")
+        } else {
+          warning("pathview package not installed; skipping KEGG plots")
+        }
+      },
+      error = function(e) {
       }
-    }, error = function(e) {
-    })
+    )
   }
-  
-  dir.create("paths") 
-  filesstrings::file.move(list.files(pattern = paste0(abrScientificName,
-                                                      "[0-9]*\\..*\\.png")), 
-                          "paths") # hsa for human
+
+  dir.create("paths")
+  filesstrings::file.move(
+    list.files(pattern = paste0(
+      abrScientificName,
+      "[0-9]*\\..*\\.png"
+    )),
+    "paths"
+  ) # hsa for human
   setwd(tmp)
   l <- list.files(file.path(outKEGG, "paths"), full.names = T)
   res_output$KEGGpaths <- l
-  
-  
-  
+
+
+
   x <- otargen::colocalisationsForGene(goi)
   x <- as.data.frame(x)
   if (nrow(x) > 0) {
     if (length(x) > 10) {
-      ind <- grep(paste(interestedTerms, collapse = "|") , 
-                  x$Trait_reported, ignore.case = TRUE)
+      ind <- grep(paste(interestedTerms, collapse = "|"),
+        x$Trait_reported,
+        ignore.case = TRUE
+      )
       message("Here are the most interested ones")
-      write_txt_xlsx(x[ind, ], outdir, 
-                     prefix = paste0("colocalisationsForGene_", goi))
+      write_txt_xlsx(x[ind, ], outdir,
+        prefix = paste0("colocalisationsForGene_", goi)
+      )
     } else {
       message("Here are the all traits associated with this gene")
-      write_txt_xlsx(x, outdir, 
-                     prefix = paste0("colocalisationsForGene_", goi))
+      write_txt_xlsx(x, outdir,
+        prefix = paste0("colocalisationsForGene_", goi)
+      )
     }
     res_output$colocalisationsForGene <- x
   }
-  
-  
-  
-  
+
+
+
+
   if (length(x$Study) == 1) {
-    grDevices::png(paste0(outdir, "manhattan1_", goi, ".png"), 
-                   units = "in", height = 10, width = 14, res = 200)
+    grDevices::png(paste0(outdir, "manhattan1_", goi, ".png"),
+      units = "in", height = 10, width = 14, res = 200
+    )
     otargen::plot_manhattan(otargen::manhattan(x$Study[1]))
     grDevices::dev.off()
   } else if (length(x$Study) == 2) {
-    grDevices::png(paste0(outdir, "manhattan1_", goi, ".png"), 
-                   units = "in", height = 10, width = 14, res = 200)
+    grDevices::png(paste0(outdir, "manhattan1_", goi, ".png"),
+      units = "in", height = 10, width = 14, res = 200
+    )
     otargen::plot_manhattan(otargen::manhattan(x$Study[1]))
     grDevices::dev.off()
-    
-    grDevices::png(paste0(outdir, "manhattan2_", goi, ".png"), 
-                   units = "in", height = 10, width = 14, res = 200)
+
+    grDevices::png(paste0(outdir, "manhattan2_", goi, ".png"),
+      units = "in", height = 10, width = 14, res = 200
+    )
     otargen::plot_manhattan(otargen::manhattan(x$Study[2]))
     grDevices::dev.off()
   }
-  
-  
+
+
   return(res_output)
 }
